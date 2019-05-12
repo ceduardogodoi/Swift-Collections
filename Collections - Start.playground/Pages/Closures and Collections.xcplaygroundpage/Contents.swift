52bd48aa444f3892aa043731f6978fa5776c0016
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-05-12 11:44:43 -0300

    Closures and Collections.

--- Collections - Start.playground/Pages/Closures and Collections.xcplaygroundpage/Contents.swift
+++ Collections - Start.playground/Pages/Closures and Collections.xcplaygroundpage/Contents.swift
@@ -3,17 +3,28 @@
 var names = ["Zeus", "Poseidon", "Ares", "Demeter"]
 
 // sort() & sort(by:) - Sorts in place / mutates the original
-
-
+names.sort()
+names.sort(by: { (a, b) -> Bool in
+    a > b
+})
 
 // sorted() & sorted(by:) - Returns a new collection that is sorted
+let longToShortNames = names.sorted() {
+    $0.count > $1.count
+}
 
-
+longToShortNames
+names
 
 //: `filter`
 var prices = [1.50, 10.00, 4.99, 2.30, 8.19]
 
-// TODO: Rewrite the loop below using `filter`
+//let largePrices = prices.filter { price -> Bool in
+//    return price > 5
+//}
+
+let largePrices = prices.filter { $0 > 5 }
+largePrices
 
 // `filter` as a `for` loop
 var arrayForFilteredResults: [Double] = []
@@ -23,7 +34,9 @@ for price in prices where price > 5 {
 arrayForFilteredResults
 
 //: `map`
-// TODO: Rewrite the loop below using `map`
+let salesPrices = prices.map { price -> Double in
+    return price * 0.9
+}
 
 // `map` as a `for` loop
 var arrayForSalePrices: [Double] = []
@@ -33,7 +46,12 @@ for price in prices {
 arrayForSalePrices
 
 //: `reduce`
-// TODO: Rewrite the loop below using `reduce`
+//let sum = prices.reduce(0) { result, price -> Double in
+//    return result + price
+//}
+
+// The plus signal needs two numbers to do the add operation, so then the compiler understands that it needs to sum the left variable, 0 in this case, with the value of the right, the value of first position of the array and so on, until the array values ends
+let sum = prices.reduce(0, +)
 
 // `reduce` as a `for` loop - `array` version
 var doubleForSum = 0.0
@@ -45,7 +63,9 @@ doubleForSum
 
 var stock = [1.50: 5, 10.00: 2, 4.99: 20, 2.30: 5, 8.19: 30]
 
-// TODO: Rewrite the loop below using `reduce(into)`
+let stockSum = stock.reduce(into: []) { result, pair in
+    result.append(pair.key * Double(pair.value))
+}
 
 // `reduce` as a `for` loop - `dictionary` version
 var arrayForStockValues: [Double] = []
@@ -59,7 +79,9 @@ arrayForStockValues
 //: `compactMap` & `flatMap`
 let userInput = ["meow!", "15", "37.5", "seven"]
 
-// TODO: Rewrite the loop below using `compactMap`
+userInput.compactMap { input in
+    Int(input)
+}
 
 // `compactMap` as a `for` loop
 var arrayForValidInput: [Int] = []
@@ -75,8 +97,23 @@ arrayForValidInput
 let arrayOfDwarfArrays = [["Sleepy", "Grumpy", "Doc"], ["Thorin", "Nori"]]
 
 // TODO: Use `flatMap` and `filter` to create a new array that only contains the dwarves with names starting after "M"
+let dwarversAfterM = arrayOfDwarfArrays.flatMap { array -> [String] in
+    return array.filter { dwarf -> Bool in
+        dwarf > "M"
+    }
+}
+let sortedDwarves = dwarversAfterM.sorted()
 
 // `flatMap` as a `for` loop - I've leave this one as a bonus challenge!
+var dwarversAfterMForLoop: [String] = []
+for array in arrayOfDwarfArrays {
+    for dwarf in array {
+        if dwarf > "M" {
+            dwarversAfterMForLoop.append(dwarf)
+        }
+    }
+}
+dwarversAfterMForLoop.sort()
 
 
 //: [Next](@next)
